1baf91ba2bae2198c7084c8c5fa31937c42a529c
Maryam Tehrani <maryamtehranizadeh@yahoo.com>
2024-08-27 00:54:13 +0100

    counter

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,17 +2,43 @@ import { useState } from "react";
 
 function Counter() {
     const [counter, setCounter] = useState(0);
+    const [isShow, setIsShow] = useState(true);
+
+    const showHandler=()=>{
+        setIsShow(true)
+    }
+
+    const hideHandler=()=>{
+        setIsShow(false)
+    }
      
     const increaseHandler = ()=>{
-          setCounter(counter+1)  
+          setCounter(counter=>counter+1)  
     }
+    const decreaseHandler = ()=>{
+        setCounter(counter=>counter-1)  
+    }
+    const stopHandler = ()=>{
+        setCounter("Stopped")  
+    }
+
+
     return (
-        <div>
-            <hr />
-            <h1>counter</h1>
-            <h3>{counter}</h3>
-            <button onClick={increaseHandler}>Increase</button>
-        </div>
+        <>
+            <button onClick={showHandler}>Show</button>
+            <button onClick={hideHandler}>Hide</button>
+            {
+                isShow && <div>
+                <hr />
+                    <h1>counter</h1>
+                    <h3>{counter}</h3>
+                    <button onClick={increaseHandler}>Increase</button>
+                    <button onClick={decreaseHandler}>Decrease</button>
+                    <button onClick={stopHandler}>Stop</button>
+                </div>
+            }
+            
+        </>
   )
 }
 
